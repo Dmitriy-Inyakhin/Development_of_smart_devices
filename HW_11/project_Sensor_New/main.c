5d47236a64afb4ee143c643c8315c9a106491bc4
Papa <dmitriy010379@yandex.ru>
2025-12-17 22:02:33 +0300

    HW_11-17/12/2025

--- HW_11/project_Sensor_New/main.c
+++ HW_11/project_Sensor_New/main.c
@@ -1,27 +1,135 @@
 // main.c
 
+#include <stdio.h>
+#include <unistd.h>
 #include "temp_api.h"
+#include <stdlib.h>
 
-int main(void) {
-    TempData data = {0};  // count = 0
+// функция парсинга вводимых параметров
+static void parse_arguments(int argc, char *argv[], uint16_t *p_yyyy, uint8_t *p_mm);
 
+int main(int argc, char *argv[])
+{
+    sensor_data data = {0}; // count = 0 - обязательная запись, иначе ни чего работать не будет!
+
+    uint16_t yyyy = 0;
+    uint8_t mm = 0;
+    parse_arguments(argc, argv, &yyyy, &mm);
+    
+    printf("%d, %02d\n", yyyy, mm);
     // Добавим несколько записей вручную (как пример)
-    add_record(&data, 2025, 3, 10, 8, 30, 12);
-    add_record(&data, 2025, 3, 10, 14, 45, 18);
-    add_record(&data, 2025, 3, 11, 7, 15, 9);
-    add_record(&data, 2025, 4, 5, 10, 0, -2);
-    add_record(&data, 2025, 4, 6, 16, 20, 21);
+    add_record(&data, 2024, 4, 10, 14, 45, 18);
+    add_record(&data, 2024, 4, 11, 7, 15, 9);
+    add_record(&data, 2024, 4, 10, 8, 30, 12);
+    add_record(&data, 2024, 5, 10, 14, 45, 21);
+    add_record(&data, 2024, 5, 11, 7, 15, 8);
+
+    // add_record(&data, 2025, 2, 10, 8, 30, 12);
+    // add_record(&data, 2025, 3, 10, 14, 45, 18);
+    // add_record(&data, 2025, 3, 11, 7, 15, 9);
+    // add_record(&data, 2025, 4, 5, 10, 0, -2);
+    // add_record(&data, 2025, 4, 6, 16, 20, 21);
 
     // Выводим всё
     print_all(&data);
 
-    // Статистика (заглушки)
-    print_monthly_stats(&data, 2025, 3);
-    print_monthly_stats(&data, 2025, 4);
-    print_yearly_stats(&data, 2025);
-	
-	int a = 0;
-	scanf("%d", &a);
+    // Статистика
+    print_monthly_stats(&data, 2024, 4);
+
+    // print_yearly_stats(&data, 2025);
+    print_yearly_stats(&data, 2026);
+
+    if (sorByDate(&data) != 0)
+    {
+        fprintf(stderr, "Error sorting by date\n");
+        return 1;
+    }
+    // Выводим всё
+    print_all(&data);
+
+    if (sorByTemp(&data) != 0)
+    {
+        fprintf(stderr, "Error sorting by temperature\n");
+        return 1;
+    }
+    // Выводим всё
+    print_all(&data);
+
+    int a = 0;
+    scanf("%d", &a);
 
     return 0;
+}
+
+// функция парсинга вводимых параметров
+static void parse_arguments(int argc, char *argv[], uint16_t *p_yyyy, uint8_t *p_mm)
+{
+    int rez = 0;
+    uint16_t yyyy = 0;
+    uint8_t mm = 0;
+
+    while ((rez = getopt(argc, argv, "hf::y::m::")) != -1)
+    {
+        switch (rez)
+        {
+        case 'h':
+            print_help();
+            break;
+        case 'f':
+            printf("found argument \"f = %s.csv\"\n", optarg);
+            break;
+        case 'y':
+        {
+            if (optarg == NULL)
+            {
+                printf("argument \"y\" default - 2025\n");
+                yyyy = 2025;
+            }
+            else
+            {
+                long y = strtol(optarg, NULL, 10);
+                if (y >= 0 && y < 100)
+                {
+                    yyyy = 2000 + (uint16_t)y;
+                }
+                else
+                {
+                    yyyy = (uint16_t)y;
+                }
+            }
+            break;
+        }
+        case 'm':
+        {
+            if (optarg == NULL)
+            {
+                // printf("argument \"m\" default - 01\n");
+                mm = 1;
+            }
+            else
+            {
+                int tmp = atoi(optarg);
+                if (tmp < 1 || tmp > 12)
+                {
+                    fprintf(stderr, "Warning: month %d out of range [1..12], using 1\n", tmp);
+                    mm = 1;
+                }
+                else
+                {
+                    mm = (uint8_t)tmp;
+                }
+            }
+            break;
+        }
+        case '?':
+            printf("Error found !\n");
+            break;
+        }
+    }
+
+    // Возвращаем результаты через указатели
+    if (p_yyyy)
+        *p_yyyy = yyyy;
+    if (p_mm)
+        *p_mm = mm;
 }
--- HW_11/project_Sensor_New/temp_api.h
+++ HW_11/project_Sensor_New/temp_api.h
@@ -3,36 +3,53 @@
 #ifndef TEMP_API_H
 #define TEMP_API_H
 
-// Структура одной записи температуры
-typedef struct {
-    int year;     // 4 цифры, например: 2025
-    int month;    // 1–12
-    int day;      // 1–31
-    int hour;     // 0–23
-    int minute;   // 0–59
-    int temp;     // от -99 до +99
-} TempRecord;
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>   // для getopt(), optarg, optind, и т.д.
 
-// Массив записей (можно заменить на динамический при желании)
+// Максимальное количество записей
 #define MAX_RECORDS 100
 
-typedef struct {
-    int count;
-    TempRecord records[MAX_RECORDS];
-} TempData;
+// Структура одной записи температуры
+typedef struct
+{
+    uint16_t year;  // 2 байта
+    uint8_t month;  // 1 байт (1–12)
+    uint8_t day;    // 1 байт (1–31)
+    uint8_t hour;   // 1 байт (0–23)
+    uint8_t minute; // 1 байт (0–59)
+    int8_t t;       // 1 байт (−99…+99), диапазон укладывается в int8_t
+
+} sensor;
+
+typedef struct
+{
+    uint32_t number;
+    sensor info[MAX_RECORDS];
+} sensor_data;
 
 // === Прототипы функций (заглушки) ===
 
-// Добавить запись (для примера)
-void add_record(TempData *data, int year, int month, int day, int hour, int minute, int temp);
+// Функция добавления записи
+int add_record(sensor_data *data, uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, int8_t temp);
 
-// Вывод всех записей
-void print_all(const TempData *data);
+// Функция печати всех записей
+void print_all(const sensor_data *data);
 
-// Статистика по месяцу (заглушка)
-void print_monthly_stats(const TempData *data, int year, int month);
+// Выводим статистику за месяц года
+int print_monthly_stats(const sensor_data *data, int year, int month);
 
-// Статистика по году (заглушка)
-void print_yearly_stats(const TempData *data, int year);
+// Выводим статистику за год
+int print_yearly_stats(const sensor_data *data, int year);
+
+// Функция сортировки по температуре
+int sorByTemp(sensor_data *data);
+
+// Функция сортировки по дате
+int sorByDate(sensor_data *data);
+
+// функция печати "хелпа"
+void print_help();
+
 
 #endif
